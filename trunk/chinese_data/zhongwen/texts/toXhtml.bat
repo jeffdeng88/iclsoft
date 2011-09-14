73d64e8762ef818c0ce1b7149eebfc60e3bd9157
dengx01@gmail.com <dengx01@gmail.com@74a64a1e-f24d-0410-b80f-25bf1cf6c937>
2011-09-14 04:41:28 +0000

    -o, --output does not seem to work

--- trunk/chinese_data/zhongwen/texts/toXhtml.bat
+++ trunk/chinese_data/zhongwen/texts/toXhtml.bat
@@ -1,1 +1,3 @@
-xsltproc  http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl -o V3L1_text.html V3L1_text.xml
+
+# --output V3L1_text.html
+xsltproc  http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L1_text.xml>V3L1_text.html
