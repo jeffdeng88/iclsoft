c3e48e1ecda9f4e46566e64d201bbfc9f37b2362
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2011-09-14 23:43:55 +0000

    more formatting

--- chinese_data/zhongwen/texts/toXhtml.bat
+++ chinese_data/zhongwen/texts/toXhtml.bat
@@ -1,3 +1,7 @@
 
 # --output V3L1_text.html
-xsltproc  http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L1_text.xml>V3L1_text.html
+xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L1_text.xml>V3L1_text.html
+
+type V3L1_text.html
+
+rem xsltproc  http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl *.xml>V3L1_text.html
