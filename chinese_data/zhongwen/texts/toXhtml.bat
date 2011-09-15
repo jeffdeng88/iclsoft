822140244322153f5b4a00235ef26c62055e0d1a
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2011-09-15 00:47:23 +0000

    conform to docbook spec

--- chinese_data/zhongwen/texts/toXhtml.bat
+++ chinese_data/zhongwen/texts/toXhtml.bat
@@ -1,7 +1,10 @@
 
 # --output V3L1_text.html
-xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L1_text.xml>V3L1_text.html
+rem xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L1_text.xml>V3L1_text.html
 
-type V3L1_text.html
+rem type V3L1_text.html
 
 rem xsltproc  http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl *.xml>V3L1_text.html
+
+
+xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L2_text.xml>V3L2_text.html
