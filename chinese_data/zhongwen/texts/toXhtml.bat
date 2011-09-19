8b62cc53f44a87bf917c78077f6f0a1597b4d0c7
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2011-09-19 03:19:00 +0000

    adding command line arg to bat script; adding V5L1 test

--- chinese_data/zhongwen/texts/toXhtml.bat
+++ chinese_data/zhongwen/texts/toXhtml.bat
@@ -7,4 +7,6 @@ rem type V3L1_text.html
 rem xsltproc  http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl *.xml>V3L1_text.html
 
 
-xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L2_text.xml>V3L2_text.html
+#xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl V3L2_text.xml>V3L2_text.html
+echo "Arg1 [%1]"
+xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl %1_text.xml>%1_text.html
